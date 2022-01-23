8c187739c7ff3d0205b00d84ef416619eab6731e
Mustafa <mustafa31112@gmail.com>
2022-01-23 20:06:00 +0000

    added promises and tested it with internet disconnection

---
+++ index2.js
@@ -0,0 +1,22 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+
+fetchMyIP()
+  .then(body => console.log(body));
+  fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(body => console.log(body));
+  fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  // .catch((error) => {
+  //   console.log("It didn't work: ", error.message);
+  // });
